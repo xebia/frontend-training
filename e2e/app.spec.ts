e88de6386a75151a3988a26db9ca47a340b0a573
Frank van Wijk <info@frankvanwijk.nl>
2024-02-09 08:32:25 +0100

    Extend E2E tests

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -1,7 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has home page with list of pokémon', async ({ page }) => {
   await page.goto('/');
 
   await expect(page.getByRole('heading', { level: 1, name: 'Pokémon' })).toBeVisible();
+
+  await expect(page.getByRole('link', { name: 'bulbasaur' })).toBeVisible();
+});
+
+test('navigates to pokemon detail', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('link', { name: 'ivysaur' }).click();
+
+  await page.waitForURL('**/pokemon/2/');
+
+  await expect(
+    page.getByRole('heading', { level: 1, name: 'Bulbasaur' }) // Mock name
+  ).toBeVisible();
+  await expect(page.getByText('ID: 2')).toBeVisible();
 });
